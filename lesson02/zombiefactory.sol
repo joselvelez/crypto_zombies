487b7089c3c7b5e4eb1518daac1bbb1881e2adc4
joselvelez <joselvelez@protonmail.com>
2021-04-29 15:55:16 -0500

    lesson 02

---
+++ lesson02/zombiefactory.sol
@@ -0,0 +1,41 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    // mapping key=>value pairs
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) internal {
+        // Solidity 0.6.2 breaks this. Use array.length instead now
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender] ++;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    // helper function to generate a pseudo-random dna value that is the length of the dnaDigits
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
